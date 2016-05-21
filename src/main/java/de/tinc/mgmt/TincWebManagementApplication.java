83f9331162e3d4054ea1a33fd1296dd5fc685d69
Tobias Knapp <t.knapp@fh-bingen.de>
2016-05-21 13:14:23 +0200

    Init Spring Boot

---
+++ src/main/java/de/tinc/mgmt/TincWebManagementApplication.java
@@ -0,0 +1,12 @@
+package de.tinc.mgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TincWebManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TincWebManagementApplication.class, args);
+	}
+}
